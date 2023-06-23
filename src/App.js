57ee5f59f75ddac8d610da1056603fc1517ed213
Gianni Narracci <gnarracci@gmail.com>
2023-06-23 05:38:29 -0400

    Configuracion del Restante del Presupuesto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
   // Definir el state
@@ -8,14 +10,24 @@ function App() {
   const [restante, guardarRestante] = useState(0);
   const [mostrarpregunta, actualizarPregunta] = useState(true);
   const [gastos, guardarGastos] = useState([]);
+  const [gasto, guardarGasto] = useState({});
+  const [creargasto, guardarCrearGasto] = useState(false);
 
-  // Cuando agreguemos un nuevo gasto
-  const agregarNuevoGasto = gasto => {
-    guardarGastos([
-      ...gastos,
-      gasto
-    ])
-  }
+  // UseEffect que actualiza el restante
+  useEffect(() => {
+
+    // Agrega el nuevo presupuesto
+    if (creargasto) {
+      guardarGastos([...gastos, gasto]);
+    }
+
+    // Resta del Presupuesto Actual
+    const presupuestoRestante = restante - gasto.cantidad;
+    guardarRestante(presupuestoRestante);
+
+    // Resetear a false
+    guardarCrearGasto(false);
+  }, [gasto]);
 
   return (
     <div className="container">
@@ -31,9 +43,18 @@ function App() {
           ) : (
             <div className="row">
               <div className="one-half column">
-                <Formulario agregarNuevoGasto={agregarNuevoGasto}/>
+                <Formulario
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
               </div>
-              <div className="one-half column">2</div>
+              <div className="one-half column">
+                <Listado gastos={gastos} />
+                <ControlPresupuesto
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
+              </div>
             </div>
           )}
         </div>
